fbbe1aab3ede84e00a6772c8094c462b3cfea297
Mamonha <mamonha302@gmail.com>
2024-02-04 21:45:45 -0300

    Creating user migration to create users table

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,8 +14,11 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('email')->unique();
+            $table->string('username');
+            $table->string('email')->unique()->nullable();
+            $table->string('phone_number')->unique()->nullable();
             $table->timestamp('email_verified_at')->nullable();
+            $table->string('profile_picture_url')->nullable();
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
