3409d13762b3bef4c48d8aab4960902ef89d74ec
Mamonha <mamonha302@gmail.com>
2024-02-12 16:00:03 -0300

    Creating login method/ generate access token

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -5,15 +5,25 @@ namespace App\Http\Controllers;
 use App\Http\Controllers\Controller;
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
 
 class AuthController extends Controller
 {
 
-    public function login()
+    public function login(Request $request)
     {
+        $credentials = $request->only('email', 'password');
 
+        if (Auth::attempt($credentials)) {
+            $user = Auth::user();
+            $token = $user->createToken('instagram-db')->accessToken;
+            
+            return response()->json(['success' => 'User logged successfully!','token' => $token], 200);
+        } else {
+            return response()->json(['error' => 'Unauthorized'], 401);
+        }
 
     }
 
