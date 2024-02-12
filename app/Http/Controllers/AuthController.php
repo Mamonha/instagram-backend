d96ff4ba44480c0accf51bd0130ebf8ca6e15b64
Mamonha <mamonha302@gmail.com>
2024-02-12 15:23:12 -0300

    Refactor: register method finalized

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -3,27 +3,43 @@
 namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
-use App\Http\Requests\StoreUser;
 use App\Models\User;
 use Illuminate\Http\Request;
-use Throwable;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
 
 class AuthController extends Controller
 {
 
     public function login()
     {
+
+
     }
 
 
-    public function register(StoreUser $request)
+    public function register(Request $request)
     {
-        try {
-            $data = $request->validated();
+        $validator = Validator::make($request->all(), [
+            'name'                => 'required|max:255',
+            'username'            => 'required|unique:users|max:255',
+            'email'               => 'nullable|email|unique:users|max:255',
+            'phone_number'        => 'nullable|unique:users|max:20',
+            'password'            => 'required|min:6',
+            'profile_picture_url' => 'nullable|url|max:255',
+        ], [
+            'email.unique'        => 'Already registered user with this email!',
+            'username.unique'     => 'Already registered user with this username!',
+            'phone_number.unique' => 'Already registered user with this phone!',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json($validator->errors(), 409);
+        } else {
+            $data = $validator->validated();
+            $data['password'] = Hash::make($data['password']);
             User::create($data);
-            return response()->json(['success' => 'User created successfully'], 201);
-        } catch (Throwable $err) {
-            return response()->json(['error' => $err->validator->errors()], 422);
+            return response()->json(['success' => 'User registered successfully'], 201);
         }
     }
 }
